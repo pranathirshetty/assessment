6336da77f1ff757bf92687cadeb0037ad4f76fde
Pranathi <pranathishetty2502@gmail.com>
2025-08-13 11:38:44 +0530

    Assesssment

--- assessment3.js
+++ assessment3.js
@@ -1,13 +1,13 @@
-
+const h=document.getElementById("image");
 for(let i=5;i<15;i++)
 { 
-let a= new Image(300,300);
+let a= new Image(350,325);
 a.src=`https://via.assets.so/movie.png?id=${i}&q=95&w=360&h=360&fit=fill`;
 a.alt="Image not found";
 a.id=i;
 a.style.border="2px solid black";
 a.style.padding="10px 10px";
-document.body.appendChild(a);
+h.appendChild(a);
 }
 
 /*for(let i=5;i<15;i++)
